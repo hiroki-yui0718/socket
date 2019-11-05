a2e47e0eb572ec1648f46260333cbf9d0731072b
広記 由井 <173224@ocsjoho.onmicrosoft.com>
2019-11-05 18:49:46 +0900

    Date Format Test

--- src/Sample.java
+++ src/Sample.java
@@ -1,9 +1,20 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello!");
+		LocalDateTime t1 = LocalDateTime.now();
+		LocalDateTime t2 = LocalDateTime.of(2019,11,5,18,22,41,604);
+		Duration d = Duration.between(t2, t1);
+		int sec = 1500;
+        int hour = sec / 3600;
+        int min = (sec%3600) / 60;
+        sec = sec % 60;
+        LocalTime t3 = LocalTime.of(hour,min,sec);
+		System.out.println(t3.toString());
 	}
 
 }
