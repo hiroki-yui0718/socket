6f0a24a4ab7fccd4e8b189238e5823836c96f8cf
広記 由井 <173224@ocsjoho.onmicrosoft.com>
2019-11-11 21:32:17 +0900

    Test! Yeah! 2019/11/11

--- src/ClientSide.java
+++ src/ClientSide.java
@@ -4,18 +4,20 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class ClientSide {
-	public String runSample(String line) {
+/**
+ * ソケット通信(クライアント側)
+ */
+class ClientSide {
+	void runSample() {
 
 		Socket cSocket = null;
 		BufferedReader csInput = null;
 		PrintWriter writer = null;
 		BufferedReader reader = null;
-		String send = null;
 
 		try{
 			//IPアドレスとポート番号を指定してクライアント側のソケットを作成
-			cSocket = new Socket("127.0.0.1", 8765);
+			cSocket = new Socket("192.168.3.10", 8765);
 
 			//クライアント側での入力用
 			csInput = new BufferedReader
@@ -29,12 +31,17 @@ public class ClientSide {
 			reader = new BufferedReader
 					(new InputStreamReader
 							(cSocket.getInputStream()));
+
+			//無限ループ　byeの入力でループを抜ける
+			String line = null;
 				System.out.println("-------------------");
 				System.out.println("偶数の数値を入力して下さい");
 				System.out.println("-------------------");
-				line = csInput.readLine();
-				writer.println(line);
 
+				line = csInput.readLine();
+
+				//送信用の文字を送信
+				writer.println(line);
 
 		}catch(Exception e){
 			e.printStackTrace();
@@ -55,10 +62,12 @@ public class ClientSide {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-	        System.out.println("クライアント側終了です");
-
+			System.out.println("クライアント側終了です");
 		}
-		return send;
+	}
 
-		}
+	public static void main(String[] args) {
+		ClientSide s2 = new ClientSide();
+		s2.runSample();
+	}
 }
--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,5 @@
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 public class Sample {
 
@@ -9,16 +8,36 @@ public class Sample {
 		LocalDateTime t1 = LocalDateTime.now();
 		LocalDateTime t2 = LocalDateTime.of(2019,11,5,18,22,41,604);
 		Duration d = Duration.between(t2, t1);
-		int sec = 1500;
-        int hour = sec / 3600;
-        int min = (sec%3600) / 60;
-        sec = sec % 60;
-        LocalTime t3 = LocalTime.of(hour,min,sec);
-		System.out.println(t3.toString());
-
+//		int sec = 30;
+//      int hour = sec / 3600;
+//      int min = (sec%3600) / 60;
+//      sec = sec % 60;
+		int sec = 30;
+		int hour = 120;
+		int min = 21;
+        String date = hour + ":" + min + ":" + sec;
+        System.out.println(sumTime(date,30));
 		System.out.println(t1.getYear());
 		System.out.println(t1.getMonthValue());
 		System.out.println(t1.getDayOfMonth());
+		System.out.println("勤務時間" + date);
+	}
+	public static String monthTime(int sec) {
+		int hour = sec / 3600;
+		int min = (sec%3600) / 60;
+		sec = sec % 60;
+		String str = hour + ":" + min +":" +sec;
+		return str;
+	}
+	public static String sumTime(String monthTime,int sec) {
+		String[] time = new String[3];
+		time = monthTime.split(":", 0);
+		int sum = Integer.parseInt(time[0]) * 3600;
+		sum += Integer.parseInt(time[1]) * 60;
+		sum += Integer.parseInt(time[2]);
+		sum += sec;
+		String str = monthTime(sum);
+		return str;
 	}
 
 }
